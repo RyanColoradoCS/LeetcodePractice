082169bcbfe1960c0e88c06fcd05ab6000b0d33e
Ryan Young <rmyoung04@gmail.com>
2024-10-05 14:42:58 -0500

    Update MaxAverageSubarray1_643_Kadane.py

--- Arrays_and_Strings/MaxAverageSubarray1_643_Kadane.py
+++ Arrays_and_Strings/MaxAverageSubarray1_643_Kadane.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
 
+        max_subarray = sum(nums[:k])
+        current = sum(nums[:k])
+
+        for i in range(k, len(nums)):
+            current = current + nums[i] - nums[i-k]
+            max_subarray = max(max_subarray, current)
+
+        return (max_subarray*1.0)/k
